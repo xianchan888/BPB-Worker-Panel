743359113c3e579b049d7ebf33da55c7facb071e
xianchan888 <63529203+xianchan888@users.noreply.github.com>
2024-09-16 01:54:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '86646a7f-0120-4952-808e-20bcc3056bcc';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
